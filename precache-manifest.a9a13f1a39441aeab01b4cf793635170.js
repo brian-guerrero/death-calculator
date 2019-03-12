e8b9bf69c4e1d5b3e473e5ca44d1622738a34887
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-12 22:19:26 +0000

    Deploy brian-guerrero/death-calculator to github.com/brian-guerrero/death-calculator.git:gh-pages

---
+++ precache-manifest.a9a13f1a39441aeab01b4cf793635170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae70f9836d503bea99c3",
+    "url": "/death-calculator/static/css/main.59b1fca6.chunk.css"
+  },
+  {
+    "revision": "ae70f9836d503bea99c3",
+    "url": "/death-calculator/static/js/main.ae70f983.chunk.js"
+  },
+  {
+    "revision": "a81678a01667e44bd685",
+    "url": "/death-calculator/static/js/runtime~main.a81678a0.js"
+  },
+  {
+    "revision": "8ba509ff0cbfd03b066b",
+    "url": "/death-calculator/static/js/2.8ba509ff.chunk.js"
+  },
+  {
+    "revision": "b7f95c3f2f23a5b84b8237f73c933f96",
+    "url": "/death-calculator/index.html"
+  }
+];
